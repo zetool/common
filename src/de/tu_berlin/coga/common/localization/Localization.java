45682c4f37634b068d0d7aa978cce2d35cd1158d
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2014-04-22 16:40:16 +0000

    Missing LocalizationManager files.

---
+++ src/de/tu_berlin/coga/common/localization/Localization.java
@@ -0,0 +1,45 @@
+
+package de.tu_berlin.coga.common.localization;
+
+import java.util.List;
+import java.util.Locale;
+
+/**
+ *
+ * @author Jan-Philipp Kappmeier
+ */
+public interface Localization {
+
+	/**
+	 * Returns a localized string assigned to a key. The currently set prefix is
+	 * added to the key.
+	 * 
+	 * @param key the key specifying the loaded string
+	 * @return the localized string
+	 */
+	String getString( String key );
+
+	/**
+	 * Sets a prefix that is added to the key if {@link #getString(String)} is used.
+	 * @param prefix the prefix that is added
+	 */
+	void setPrefix( String prefix );
+
+	/**
+	 * Returns a localized string assigned to a key. No prefix is added to the key.
+	 * @param key the key specifying the loaded string
+	 * @return the localized string
+	 */
+	String getStringWithoutPrefix( String key );
+
+	void addSupportedLocale( Locale locale );
+
+	List<Locale> getSupportedLocales();
+
+	/**
+	 * Clears any prefix.
+	 * @see #setPrefix(java.lang.String)
+	 */
+	void clearPrefix();
+	
+}
