12f609e555e90f6f15baa29fa28705ad84230e92
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2015-04-19 01:57:31 +0200

    An additional unit test.

--- test/org/zetool/common/util/FormatterTest.java
+++ test/org/zetool/common/util/FormatterTest.java
@@ -61,4 +61,18 @@ public class FormatterTest {
 		assertEquals( TimeUnits.PicoSeconds, ret.getUnit() );
 
 	}
+  
+  /**
+   * Tests if the formatter for percents uses the descripted number of digits
+   */
+  @Test
+  public void formatPercentTest() {
+    assertEquals( "Testing negative", 6, Formatter.formatPercent( -0.01 ).length() );
+    assertEquals( "Testing 0", 5, Formatter.formatPercent( 0 ).length() );
+    assertEquals( "Testing float", 6, Formatter.formatPercent( 0.5f ).length() );
+    assertEquals( "a third", 6, Formatter.formatPercent( 0.3 ).length() );
+    assertEquals( "Testing double", 6, Formatter.formatPercent( 0.5d ).length() );
+    assertEquals( "Testing 1", 7, Formatter.formatPercent( 1 ).length() );
+    assertEquals( "one and a third", 7, Formatter.formatPercent( 1.3 ).length() );
+  }
 }
