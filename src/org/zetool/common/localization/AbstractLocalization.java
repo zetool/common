01a7c8e7ed9eccac8cc7a3bf9666c5d44eaf5e1d
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2015-09-17 23:52:01 +0200

    Formatting, bundle name corrected.

--- src/org/zetool/common/localization/AbstractLocalization.java
+++ src/org/zetool/common/localization/AbstractLocalization.java
@@ -13,7 +13,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-
 package org.zetool.common.localization;
 
 import java.util.ArrayList;
@@ -24,99 +23,109 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 /**
- * {@code Loalization} is a class that provides the ability to localize an
- * application. It supports access to files containing localized strings and
- * number format conventions.
+ * {@code Loalization} is a class that provides the ability to localize an application. It supports access to files
+ * containing localized strings and number format conventions.
+ *
  * @author Jan-Philipp Kappmeier
  */
 public abstract class AbstractLocalization implements Localization {
 
-	/** The resource bundle that is used by this localization instance. */
-	private final String bundleName;
-	/** The resource bundle that is selected (containing the localized strings). */
-	private ResourceBundle bundle;
-	/** A prefix that is added to the keys for localized strings. */
-	private String prefix = "";
-	/** Indicates if only the key is returned, if an unknown key was used. Otherwise some larger text is returned. */
-	private final boolean returnKeyOnly = true;
+    /**
+     * The resource bundle that is used by this localization instance.
+     */
+    private final String bundleName;
+    /**
+     * The resource bundle that is selected (containing the localized strings).
+     */
+    private ResourceBundle bundle;
+    /**
+     * A prefix that is added to the keys for localized strings.
+     */
+    private String prefix = "";
+    /**
+     * Indicates if only the key is returned, if an unknown key was used. Otherwise some larger text is returned.
+     */
+    private final boolean returnKeyOnly = true;
 
-	private final List<Locale> supportedLocales = new ArrayList<>();
+    private final List<Locale> supportedLocales = new ArrayList<>();
 
-	/**
-	 * Creates a new instance of the singleton and initializes with the default locale of the system.
-	 * @param bundleName the name of the ressource bundle for the localization
-	 * @throws MissingResourceException if no resource bundle for the default system locale is found
-	 */
-	AbstractLocalization( String bundleName, Locale currentLocale ) {
-		this.bundleName = bundleName;
-		bundle = ResourceBundle.getBundle( bundleName, currentLocale );
-	}
+    /**
+     * Creates a new instance of the singleton and initializes with the default locale of the system.
+     *
+     * @param bundleName the name of the ressource bundle for the localization
+     * @throws MissingResourceException if no resource bundle for the default system locale is found
+     */
+    AbstractLocalization(String bundleName, Locale currentLocale) {
+        this.bundleName = bundleName;
+        bundle = ResourceBundle.getBundle(bundleName, currentLocale);
+    }
 
+    @Override
+    public void addSupportedLocale(Locale locale) {
+        supportedLocales.add(locale);
+    }
 
-	@Override
-	public void addSupportedLocale( Locale locale ) {
-		supportedLocales.add( locale );
-	}
+    @Override
+    public List<Locale> getSupportedLocales() {
+        return Collections.unmodifiableList(supportedLocales);
+    }
 
-  @Override
-	public List<Locale> getSupportedLocales() {
-		return Collections.unmodifiableList( supportedLocales );
-	}
+    /**
+     * Returns a localized string assigned to a key. The currently set prefix is added to the key.
+     *
+     * @param key the key specifying the loaded string
+     * @return the localized string
+     */
+    @Override
+    public final String getString(String key) {
+        try {
+            return key.isEmpty() ? "" : bundle.getString(prefix + key);
+        } catch (MissingResourceException ex) {
+            return returnKeyOnly ? prefix + key : "Unknown Language key: '" + prefix + key + "'";
+        }
+    }
 
-	/**
-	 * Returns a localized string assigned to a key. The currently set prefix is added to the key.
-	 * @param key the key specifying the loaded string
-	 * @return the localized string
-	 */
-	@Override
-	public final String getString( String key ) {
-		try {
-			return key.isEmpty() ? "" : bundle.getString( prefix + key );
-		} catch( MissingResourceException ex ) {
-			return returnKeyOnly ? prefix + key : "Unknown Language key: '" + prefix + key + "'";
-		}
-	}
+    /**
+     * Returns a localized string assigned to a key. No prefix is added to the key.
+     *
+     * @param key the key specifying the loaded string
+     * @return the localized string
+     */
+    @Override
+    public final String getStringWithoutPrefix(String key) {
+        try {
+            return key.isEmpty() ? "" : bundle.getString(key);
+        } catch (MissingResourceException ex) {
+            return returnKeyOnly ? key : "Unknown Language key: '" + key + "'";
+        }
+    }
 
-	/**
-	 * Returns a localized string assigned to a key. No prefix is added to the key.
-	 * @param key the key specifying the loaded string
-	 * @return the localized string
-	 */
-	@Override
-	public final String getStringWithoutPrefix( String key ) {
-		try {
-			return key.isEmpty() ? "" : bundle.getString( key );
-		} catch( MissingResourceException ex ) {
-			return returnKeyOnly ? key : "Unknown Language key: '" + key + "'";
-		}
-	}
+    /**
+     * Sets a prefix that is added to the key if {@link #getString(String)} is used.
+     *
+     * @param prefix the prefix that is added
+     */
+    @Override
+    public final void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
 
-	/**
-	 * Sets a prefix that is added to the key if {@link #getString(String)} is used.
-	 * @param prefix the prefix that is added
-	 */
-	@Override
-	public final void setPrefix( String prefix ) {
-		this.prefix = prefix;
-	}
+    /**
+     * Clears any prefix.
+     *
+     * @see #setPrefix(java.lang.String)
+     */
+    @Override
+    public final void clearPrefix() {
+        setPrefix("");
+    }
 
-	/**
-	 * Clears any prefix.
-	 * @see #setPrefix(java.lang.String)
-	 */
-  @Override
-	public final void clearPrefix() {
-		setPrefix( "" );
-	}
+    void setLocale(Locale locale) {
+        bundle = ResourceBundle.getBundle(bundleName, locale);
+    }
 
-	void setLocale( Locale locale ) {
-		bundle = ResourceBundle.getBundle( bundleName, locale );
-	}
-
-
-
-	@Override
-	public String toString() {
-		return "Localization: " + bundleName;
-	}
+    @Override
+    public String toString() {
+        return "Localization: " + bundleName;
+    }
 }
