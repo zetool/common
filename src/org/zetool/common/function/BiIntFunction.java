e0bbfc54b9228d7f96873efc4f08926954d5b7d3
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2015-11-08 17:57:54 +0100

    removed test code

--- src/org/zetool/common/function/BiIntFunction.java
+++ src/org/zetool/common/function/BiIntFunction.java
@@ -13,25 +13,15 @@ public interface BiIntFunction<R> {
     R apply(int t, int u);
 
 /**
-     * Returns a composed function that first applies this function to
-     * its input, and then applies the {@code after} function to the result.
-     * If evaluation of either function throws an exception, it is relayed to
-     * the caller of the composed function.
+     * Returns a composed function that first applies this integer bi-function to its input and
+     * afterwards applies the {@code after} function to the result.
      *
-     * @param <V> the type of output of the {@code after} function, and of the
-     *           composed function
-     * @param after the function to apply after this function is applied
-     * @return a composed function that first applies this function and then
-     * applies the {@code after} function
+     * @param <S> the type of the composed function
+     * @param after the function applied after the int bi-function
+     * @return a composed function that first applies this integer bi-function and then {@code after}
      * @throws NullPointerException if after is null
      */
-    default <V> BiIntFunction<V> andThen(Function<? super R, ? extends V> after) {
+    default <S> BiIntFunction<S> andThen(Function<? super R, ? extends S> after) {
         return (int t, int u) -> Objects.requireNonNull(after).apply(apply(t, u));
-    }
-    
-    public static void main(String[] a) {
-        Function<Double, Double> square = (Double t) -> t * t;
-        BiIntFunction<Double> sum = (int t, int u) -> (double) t + u;
-        System.out.println(sum.andThen(square).apply(3, 4));
     }
 }
