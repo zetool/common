fa1601d5a55a4d0f4fcee2c4dc5c54c17d26c943
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2016-12-04 02:26:20 +0100

    template set builder

--- src/org/zetool/common/algorithm/template/DefaultParameterTemplateSet.java
+++ src/org/zetool/common/algorithm/template/DefaultParameterTemplateSet.java
@@ -1,70 +1,23 @@
 package org.zetool.common.algorithm.template;
 
-import java.util.Collections;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
 /**
  *
  */
-public abstract class DefaultParameterTemplateSet implements ParameterTemplateSet {
-    private final Set<ParameterTemplate<?>> parameterTemplates;
+public class DefaultParameterTemplateSet implements ParameterTemplateSet {
 
-    /**
-     * Creates a new ParameterSet.
-     */
-    public DefaultParameterTemplateSet() {
-        parameterTemplates = Collections.emptySet();
-    }
+    private final Set<ParameterTemplate<?>> parameterTemplates;
 
     protected DefaultParameterTemplateSet(Set<ParameterTemplate<?>> parameterTemplates) {
         this.parameterTemplates = parameterTemplates;
     }
-    
+
     @Override
     public Iterator<ParameterTemplate<?>> iterator() {
         return parameterTemplates.iterator();
-    }
-
-    /**
-     * A shortcut for adding new parameters to this set. Supports Strings, Integers, Enumerations, Booleans, Doubles and
-     * Objects.
-     *
-     * @param name the name of the new parameter.
-     * @param description the description of the new parameter.
-     * @param value the default value of this parameter.
-     * @param params additional parameters that the constructors of the specific parameter classes support. See their
-     * description for more information.
-     */
-    public static <T> DefaultParameterTemplate addParameter(String name, String description, T value, Object... params) {
-        Class<?> type = value.getClass();
-        DefaultParameterTemplate<T> parameter;
-        if (type.equals(String.class)) {
-            if (params.length == 0 || !(params[0] instanceof String)) {
-                parameter = (DefaultParameterTemplate<T>) new StringParameterTemplate(name, description, (String) value);
-            } else {
-                parameter = (DefaultParameterTemplate<T>) new StringParameterTemplate(name, description, (String) value, (String) params[0]);
-            }
-        } else if (type.equals(Integer.class)) {
-            if (params.length != 2 || !(params[0] instanceof Integer) || !(params[1] instanceof Integer)) {
-                parameter = (DefaultParameterTemplate<T>) new IntegerParameterTemplate(name, description, (Integer) value);
-            } else {
-                parameter = (DefaultParameterTemplate<T>) new IntegerParameterTemplate(name, description, (Integer) value, (Integer) params[0], (Integer) params[1]);
-            }
-        } else if (type.equals(Boolean.class)) {
-            parameter = (DefaultParameterTemplate<T>) new BooleanParameterTemplate(name, description, (Boolean) value);
-        } else if (type.equals(Double.class)) {
-            if (params.length != 2 || !(params[0] instanceof Double) || !(params[1] instanceof Double)) {
-                parameter = (DefaultParameterTemplate<T>) new DoubleParameterTemplate(name, description, (Double) value);
-            } else {
-                parameter = (DefaultParameterTemplate<T>) new DoubleParameterTemplate(name, description, (Double) value, (Double) params[0], (Double) params[1]);
-            }
-        } else if (Enum.class.isAssignableFrom(type)) {
-            parameter = new EnumParameterTemplate(name, description, (Class<? extends Enum>) type, (Enum) value);
-        } else {
-            parameter = new DefaultParameterTemplate(name, description, value);
-        }
-        return parameter;
     }
 
     @Override
@@ -75,5 +28,77 @@ public abstract class DefaultParameterTemplateSet implements ParameterTemplateSe
         }
         return result;
     }
-    
+
+    @Override
+    public <T> T getValue(ParameterTemplate<T> template) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public <T> ValidationResult isChangeValid(ParameterTemplate<T> template, T value) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public <T> boolean contains(ParameterTemplate<T> template) {
+        return parameterTemplates.contains(template);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return parameterTemplates.isEmpty();
+    }
+
+    public static class Builder {
+
+        private final Set<ParameterTemplate<?>> sets = new HashSet<>();
+
+        /**
+         * A shortcut for adding new parameters to this set. Supports Strings, Integers,
+         * Enumerations, Booleans, Doubles and Objects.
+         *
+         * @param name the name of the new parameter.
+         * @param description the description of the new parameter.
+         * @param value the default value of this parameter.
+         * @param params additional parameters that the constructors of the specific parameter
+         * classes support. See their description for more information.
+         * @return the {@code Builder} instance
+         */
+        public <T> Builder withParameter(String name, String description, T value, Object... params) {
+            Class<?> type = value.getClass();
+            DefaultParameterTemplate<T> parameter;
+            if (type.equals(String.class)) {
+                if (params.length == 0 || !(params[0] instanceof String)) {
+                    parameter = (DefaultParameterTemplate<T>) new StringParameterTemplate(name, description, (String) value);
+                } else {
+                    parameter = (DefaultParameterTemplate<T>) new StringParameterTemplate(name, description, (String) value, (String) params[0]);
+                }
+            } else if (type.equals(Integer.class)) {
+                if (params.length != 2 || !(params[0] instanceof Integer) || !(params[1] instanceof Integer)) {
+                    parameter = (DefaultParameterTemplate<T>) new IntegerParameterTemplate(name, description, (Integer) value);
+                } else {
+                    parameter = (DefaultParameterTemplate<T>) new IntegerParameterTemplate(name, description, (Integer) value, (Integer) params[0], (Integer) params[1]);
+                }
+            } else if (type.equals(Boolean.class)) {
+                parameter = (DefaultParameterTemplate<T>) new BooleanParameterTemplate(name, description, (Boolean) value);
+            } else if (type.equals(Double.class)) {
+                if (params.length != 2 || !(params[0] instanceof Double) || !(params[1] instanceof Double)) {
+                    parameter = (DefaultParameterTemplate<T>) new DoubleParameterTemplate(name, description, (Double) value);
+                } else {
+                    parameter = (DefaultParameterTemplate<T>) new DoubleParameterTemplate(name, description, (Double) value, (Double) params[0], (Double) params[1]);
+                }
+            } else if (Enum.class.isAssignableFrom(type)) {
+                parameter = new EnumParameterTemplate(name, description, (Class<? extends Enum>) type, (Enum) value);
+            } else {
+                parameter = new DefaultParameterTemplate(name, description, value);
+            }
+            sets.add(parameter);
+            return this;
+        }
+
+        public ParameterTemplateSet build() {
+            return new DefaultParameterTemplateSet(sets);
+        }
+    }
+
 }
---
+++ test/org/zetool/common/algorithm/template/DefaultParameterTemplateSetTest.java
@@ -0,0 +1,44 @@
+package org.zetool.common.algorithm.template;
+
+import java.util.Collections;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.contains;
+import static org.junit.Assert.assertThat;
+import org.junit.Test;
+import static org.mockito.Mockito.mock;
+
+/**
+ *
+ * @author Jan-Philipp Kappmeier
+ */
+public class DefaultParameterTemplateSetTest {
+    @Test
+    public void initializeEmpty() {
+        DefaultParameterTemplateSet set = new DefaultParameterTemplateSet(Collections.emptySet());
+        
+        assertThat(set.isEmpty(), is(true));
+        assertThat(set.contains(mock(ParameterTemplate.class)), is(false));
+        
+        ParameterTemplate template = mock(ParameterTemplate.class, "mock");
+        set = new DefaultParameterTemplateSet(Collections.singleton(template));
+        assertThat(set.isEmpty(), is(false));
+        assertThat(set.contains(template), is(true));
+    }
+
+    @Test
+    public void builderTest() {
+        ParameterTemplateSet set = new DefaultParameterTemplateSet.Builder().build();
+        assertThat(set.isEmpty(), is(true));        
+    }
+
+    @Test
+    public void builderWithIntegerTest() {
+        ParameterTemplateSet set = new DefaultParameterTemplateSet.Builder().withParameter("integer", "integerTest", 3).build();
+        assertThat(set.isEmpty(), is(false));
+        ParameterTemplate<Integer> intParameter = (ParameterTemplate<Integer>) set.iterator().next();
+        assertThat(intParameter.getName(), is(equalTo("integer")));
+        assertThat(intParameter.getDescription(), is(equalTo("integerTest")));
+        assertThat(intParameter.getValues(), contains(3));
+    }
+}
